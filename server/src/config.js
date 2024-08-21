a13f7acfe1142c350fc068c6c8e18b5e5f9724c1
Pankaj Paliwal <158048038+pankaj-0301@users.noreply.github.com>
2024-08-22 01:44:57 +0530

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,6 @@
 // server/src/config.js
 module.exports = {
-    MONGODB_URI: process.env.MONGO_URI || 'mongodb+srv://<user>:<password>@cluster0.yvs1pu5.mongodb.net/crm',
+    MONGODB_URI: process.env.MONGO_URI ,
     PORT: process.env.PORT || 5001,
   };
   
